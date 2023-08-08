d057070fe2ce2c6c4f0fcfe97f8fde8b0e8ef0a7
Alex <867377995@qq.com>
2023-08-08 15:24:42 +0800

    第二次课程作业

---
+++ course2/build/CMakeFiles/kuiper_datawhale_course2.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kuiper_datawhale_course2.dir/main.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course2.dir/source/tensor.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course2.dir/test/test_create_tensor.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course2.dir/test/test_fill_reshape.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course2.dir/test/test_get_size.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course2.dir/test/test_get_values.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course2.dir/test/test_homework.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course2.dir/test/test_transform.cpp.o"
+  "kuiper_datawhale_course2"
+  "kuiper_datawhale_course2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kuiper_datawhale_course2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ course2/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /code/Kuiperdatawhale/course2
+# Build directory: /code/Kuiperdatawhale/course2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
