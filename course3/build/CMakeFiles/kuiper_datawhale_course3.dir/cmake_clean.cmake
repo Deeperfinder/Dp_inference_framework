08b9d5d48afa8c7c04b78956a861c88626db4e59
Alex <867377995@qq.com>
2023-08-09 17:44:12 +0800

    提交第三次课程作业

---
+++ course3/build/CMakeFiles/kuiper_datawhale_course3.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kuiper_datawhale_course3.dir/main.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/ir.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/load_data.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_attr.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_ir.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/runtime_op.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/store_zip.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/tensor.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/source/tensor_utils.cpp.o"
+  "CMakeFiles/kuiper_datawhale_course3.dir/test/test_ir.cpp.o"
+  "kuiper_datawhale_course3"
+  "kuiper_datawhale_course3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kuiper_datawhale_course3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
