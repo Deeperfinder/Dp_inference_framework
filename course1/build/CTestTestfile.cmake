debe1fb2962ba2d043fdb117a5f84cbb49d621f9
Bulubiu <54851226+Deeperfinder@users.noreply.github.com>
2024-09-24 11:46:18 +0800

    Delete course1 directory

--- course1/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /code/Kuiperdatawhale/course1
-# Build directory: /code/Kuiperdatawhale/course1/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
